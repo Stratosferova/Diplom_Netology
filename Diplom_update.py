b7f97cba49074a42cbd0dbecb8080ec025c7a6c2
Ekaterina Staroverova <31667302+Stratosferova@users.noreply.github.com>
2017-12-03 22:05:23 +0300

    Add files via upload

--- Diplom_update.py
+++ Diplom_update.py
@@ -1,23 +1,30 @@
+
 # coding: utf-8
+
+# Вывести список групп в ВК в которых состоит пользователь, но не
+# состоит никто из его друзей. В качестве жертвы, на ком тестировать, можно
+# использовать: https://vk.com/tim_leary
+
+# In[43]:
+
 # Вывести список групп в ВК в которых состоит пользователь, но не
 # состоит никто из его друзей. В качестве жертвы, на ком тестировать, можно
 # использовать: https://vk.com/tim_leary
 
 import requests
-from urllib.parse import urlencode
 import json
 import time
 import configparser
+
 
 config = configparser.ConfigParser()
 config.read('token.ini')
 USER_ID = config['data']['user_id']
 access_token = config['data']['token']
+#return USER_ID, access_token
 
-ERR_RATELIMIT = 6 # ошибка если обращений в секунду слишком много
-ERR_FORBID = 7 # если пользователь закрыл свои группы
-ERR_BAN = 18 # если удален или забанен
-ERR_AUTOFAILD = 5 # не работает авторизация
+
+ERR_RATELIMIT = 6 
     
 def vk_method(method, access_token, **kwargs):
     kwargs.update({
@@ -27,7 +34,7 @@ def vk_method(method, access_token, **kwargs):
     result = None
     while True:
         try:
-            print('-')
+            print('{} -'.format(time.ctime()))
             response_data = requests.get('https://api.vk.com/method/' + method, kwargs).json()
             result = response_data['response']['items']
         except requests.RequestException:
@@ -40,6 +47,7 @@ def vk_method(method, access_token, **kwargs):
         break
     return result
 
+
 def main():
     users = vk_method('friends.get', access_token, user_id=USER_ID)
     groups = vk_method('groups.get', access_token, user_id=USER_ID, extended=1, fields='id,name,members_count')
@@ -49,13 +57,13 @@ def main():
         if not user_groups:
             continue
         for group_id in user_groups:
-            try:
-                del groups_dict[group_id] 
-            except KeyError:
+            if group_id in groups_dict:
+                del groups_dict[group_id]
+            else:
                 pass
     groups = []
     for item in groups_dict.values(): 
-        groups.append({'id':item['id'], 'name':item['name'],'members_count':item['members_count']})
+        groups.append({'id': item['id'], 'name': item['name'],'members_count': item['members_count']})
     print(groups) 
     
     with open('groups.json', 'w', encoding='utf-8') as outfile:
@@ -63,3 +71,9 @@ def main():
         
 if __name__ == '__main__':
     main()
+
+
+# In[ ]:
+
+
+
